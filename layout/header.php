e6805aea9c1886ddc674032b5a6f8e5d040a47b5
Your Name <you@example.com>
2022-03-10 17:21:56 +0100

    Aggiunto menu

--- layout/header.php
+++ layout/header.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    
+
     <title>Comuni</title>
 </head>
+
 <body>
-    <header class="bg-primary mb-4">
-       <div class="container pt-4 pb-4">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="../comuni.php">Home</a>
+                    <a class="nav-link" href="../ricerca.php">Cerca</a>
+                    <a class="nav-link" href="../comuni.php">Comuni</a>
+                   
+                </div>
+            </div>
+        </div>
+    </nav>
+<header class="bg-primary mb-4">
+        <div class="container pt-4 pb-4">
             <h1 class="display-3 text-white">Comuni italiani</h1>
-       </div>
+        </div>
     </header>
+  
